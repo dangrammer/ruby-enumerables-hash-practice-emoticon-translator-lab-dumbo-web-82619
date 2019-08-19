8d81a03f82e8021fb7dcf96c4eb76a0af2030973
Dan Romans <danieljromans@gmail.com>
2019-08-19 17:38:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,12 +23,11 @@ def get_japanese_emoticon(file, emoticon)
   
     emoticon_hash.values[1].each do |key, value|
       if key == emoticon 
-        result = value
+        return value
       else
           return "Sorry, that emoticon was not found"
       end
     end
-   return result
 end
 
 def get_english_meaning
